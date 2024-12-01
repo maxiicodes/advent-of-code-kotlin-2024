72595e9391909b3fe1cbac110223ad5626161ef8
Maximilian Hofmann <maxi@maxiicodes.dev>
2024-12-01 22:07:45 +0100

    solution Day 1
    this is the first working solution, but it could probably be made more ideomatoc

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,45 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val lefts = mutableListOf<Int>()
+        val rights = mutableListOf<Int>()
+
+        for (line in input) {
+            val pair = line.substringBefore(' ') to line.substringAfterLast(' ')
+
+            lefts.add(pair.first.toInt())
+            rights.add(pair.second.toInt())
+        }
+
+        lefts.sort().also { rights.sort() }
+        val sortedList = lefts.zip(rights)
+        return sortedList.sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val lefts = mutableListOf<Int>()
+        val rights = mutableListOf<Int>()
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        for (line in input) {
+            val pair = line.substringBefore(' ') to line.substringAfterLast(' ')
+
+            lefts.add(pair.first.toInt())
+            rights.add(pair.second.toInt())
+        }
+
+        val sum = lefts.sumOf { left ->
+            val amount = rights.withIndex().filter { it.value == left }.map { it.index }.count()
+            left * amount
+        }
+
+        return sum
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11) { "Test input result was: ${part1(testInput)}" }
+    check(part2(testInput) == 31) { "Test input result was: ${part2(testInput)}" }
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
